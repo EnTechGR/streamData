990efb5de78c60289f345bebc6efa067f7303a43
unknown <marinoskouvaras@gmail.com>
2024-11-24 09:48:57 +0200

    streaming for one file

--- myApp/streamData/app.js
+++ myApp/streamData/app.js
@@ -2,11 +2,34 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var csv = require('csv-parser');
+var fs = require('fs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+// Store vessel data
+app.locals.vesselData = [];
+
+// Read and parse CSV file
+fs.createReadStream(path.join(__dirname, 'data/argosaronic_gulf_march_2020_part_1/split_files/part_1.csv'))
+  .pipe(csv())
+  .on('data', (row) => {
+    app.locals.vesselData.push({
+      timestamp: row.timestamp,
+      mmsi: row.mmsi,
+      longitude: parseFloat(row.longitude),
+      latitude: parseFloat(row.latitude),
+      ship_name: row.ship_name,
+      ship_type: row.ship_type,
+      heading: parseFloat(row.heading),
+      speed: parseFloat(row.sog)
+    });
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+  });
 
 app.use(logger('dev'));
 app.use(express.json());
--- myApp/streamData/webmap.js
+++ myApp/streamData/webmap.js
@@ -1,10 +1,11 @@
 // Import the leaflet package
 var L = require('leaflet');
+const io = require('socket.io-client');
 
 // Creates a leaflet map binded to an html <div> with id "map"
 // setView will set the initial map view to the location at coordinates
 // 13 represents the initial zoom level with higher values being more zoomed in
-var map = L.map('map').setView([43.659752, -79.378161], 20);
+var map = L.map('map').setView([37.946450, 23.640097], 20);
 
 // Adds the basemap tiles to your web map
 // Additional providers are available at: https://leaflet-extras.github.io/leaflet-providers/preview/
@@ -13,6 +14,39 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 	subdomains: 'abcd',
 	maxZoom: 21
 }).addTo(map);
+
+// Store markers
+const markers = {};
+
+// Connect to Socket.IO
+const socket = io();
+
+// Handle vessel updates
+socket.on('vesselUpdate', (vessel) => {
+    // Create or update marker
+    if (!markers[vessel.mmsi]) {
+        // Create new marker
+        markers[vessel.mmsi] = L.marker([vessel.latitude, vessel.longitude])
+            .bindPopup(`
+                Ship: ${vessel.ship_name}<br>
+                MMSI: ${vessel.mmsi}<br>
+                Type: ${vessel.ship_type}<br>
+                Speed: ${vessel.speed} knots<br>
+                Heading: ${vessel.heading}°
+            `)
+            .addTo(map);
+    } else {
+        // Update existing marker
+        markers[vessel.mmsi].setLatLng([vessel.latitude, vessel.longitude]);
+        markers[vessel.mmsi].getPopup().setContent(`
+            Ship: ${vessel.ship_name}<br>
+            MMSI: ${vessel.mmsi}<br>
+            Type: ${vessel.ship_type}<br>
+            Speed: ${vessel.speed} knots<br>
+            Heading: ${vessel.heading}°
+        `);
+    }
+});
 
 // Adds a popup marker to the webmap for GGL address
 L.circleMarker([37.946450, 23.640097]).addTo(map)
